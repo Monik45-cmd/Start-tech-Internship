f94068dab381d9b0b2dc1a7171131824349fec13
Monik45-cmd <113106818+Monik45-cmd@users.noreply.github.com>
2023-05-25 19:15:26 +0530

    Add files via upload

---
+++ sqltask.sql
@@ -0,0 +1,13 @@
+--select top 20 * from [dbo].[deliveries ];
+
+--select top 20 * from IPL_matches
+
+--select * from IPL_matches where date= '02-MAY-13'
+
+--select * from IPL_matches where result_margin>100
+
+--select *from IPL_matches where result = 'tie'order by date DESC;
+
+--select count(distinct city)'city_count' from IPL_matches;
+
+
